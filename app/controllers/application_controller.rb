1a4adadbf649a567e4cd79d2764e1e0c378c7bad
Andy Chan <andy.chan9930@gmail.com>
2022-03-04 15:59:57 -0500

    commit tree 6463c26a0f1d1f34864a11633cc34fd70417d1da

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,17 @@ class ApplicationController < Sinatra::Base
     { message: "Hello world" }.to_json
   end
 
+  get '/games' do
+    # get all the games from the database
+    games = Game.all
+    # return a JSON response with an array of all the game data
+    games.to_json
+  end
+
+  get '/games/:id' do
+    # look up the game in the database using its ID
+    game = Game.find(params[:id])
+    # send a JSON-formatted response of the game data
+    game.to_json(include: :reviews)
+  end
 end
